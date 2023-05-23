f04fd91270f6d0cd17e41d6b4e384d804d4e5dce
alphasec <95212748+alphasecio@users.noreply.github.com>
2023-05-24 06:57:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,32 +5,33 @@ from langchain.llms.openai import OpenAI
 from langchain.chains.summarize import load_summarize_chain
 
 # Streamlit app
-st.title('LangChain Text Summarizer')
+st.subheader('LangChain Text Summary')
 
 # Get OpenAI API key and source text input
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 source_text = st.text_area("Source Text", height=200)
 
-# Check if the 'Summarize' button is clicked
+# If the 'Summarize' button is clicked
 if st.button("Summarize"):
     # Validate inputs
     if not openai_api_key.strip() or not source_text.strip():
         st.error(f"Please provide the missing fields.")
     else:
         try:
-            # Split the source text
-            text_splitter = CharacterTextSplitter()
-            texts = text_splitter.split_text(source_text)
+            with st.spinner("Please wait..."):
+                # Split the source text
+                text_splitter = CharacterTextSplitter()
+                texts = text_splitter.split_text(source_text)
 
-            # Create Document objects for the texts
-            docs = [Document(page_content=t) for t in texts[:3]]
+                # Create Document objects for the texts
+                docs = [Document(page_content=t) for t in texts[:3]]
 
-            # Initialize the OpenAI module, load and run the summarize chain
-            llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
-            chain = load_summarize_chain(llm, chain_type="map_reduce")
-            summary = chain.run(docs)
+                # Initialize the OpenAI module, load and run the summarize chain
+                llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+                chain = load_summarize_chain(llm, chain_type="map_reduce")
+                summary = chain.run(docs)
 
-            # Display summary
-            st.success(summary)
+                # Display summary
+                st.success(summary)
         except Exception as e:
-            st.error(f"An error occurred: {e}")
+            st.exception(f"An error occurred: {e}")
